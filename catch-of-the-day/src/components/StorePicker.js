c6697ed8eef5aa8133242b22b9ad43c549f495f3
Mike Olasin <(none)>
2021-03-26 16:40:02 -0400

    completed lesson 11: events, refs, and this

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,21 +2,37 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-	render() {
-		return (
-			<React.Fragment>
-				<p>Fish!</p>
-				{/*
-					comment!
-				*/}
-				<form action="" className="store-selector">
-					<h2>Please enter a store</h2>
-					<input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
-					<button type="submit">Visit Store </button>
-				</form>
-			</React.Fragment>
-		);
-	}
+	// constructor() {
+	// 	super();
+	// 	console.log('gonna create component');
+
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from that input
+    console.log(this.myInput.current.value);
+
+    // 3. Change the page to /store/whatever-they-entered
+  };
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
