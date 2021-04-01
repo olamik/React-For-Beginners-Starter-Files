c10db177091298d74a83f61a020ef00b41b40808
Mike Olasin <(none)>
2021-04-01 16:52:06 -0400

    completed lesson 22: css animation

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class Order extends React.Component {
 	renderOrder = key => {
 		const fish = this.props.fishes[key];
 		const count = this.props.order[key];
+		const transitionOptions = {
+			classNames: 'order',
+			key: key,
+			timeout: { enter: 500, exit: 500 }
+		};
 
 		if (!fish) return null;
 
 		if (fish.status !== 'available') {
 			return (
-				<li key={key}>
-					Sorry {fish ? fish.name : 'fish'} is no longer available
-				</li>
+				<CSSTransition {...transitionOptions}>
+					<li key={key}>
+						Sorry {fish ? fish.name : 'fish'} is no longer available
+					</li>
+				</CSSTransition>
 			);
 		}
 
 		return (
-			<li key={key}>
-				{count} lbs {fish.name}
-				{formatPrice(count * fish.price)}
-				<button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
-			</li>
+			<CSSTransition {...transitionOptions}>
+				<li key={key}>
+					<span>
+						<TransitionGroup component="span" className="count">
+							<CSSTransition classNames="count" key={count} timeout={{ enter: 500, exit: 500 }}>
+								<span>{count}</span>
+							</CSSTransition>
+						</TransitionGroup>
+						lbs {fish.name}
+						{formatPrice(count * fish.price)}
+					</span>
+					<button onClick={() => this.props.removeFromOrder(key)}>&times;</button>
+				</li>
+			</CSSTransition>
 		);
 	}
 
@@ -41,9 +58,9 @@ class Order extends React.Component {
 		return (
 			<div className="inventory">
 				<h2>Order</h2>
-				<ul className="order">
+				<TransitionGroup component="ul" className="order">
 					{orderIds.map(this.renderOrder)}
-				</ul>
+				</TransitionGroup>
 				<div className="total">
 					Total:
 					<strong>{formatPrice(total)}</strong>
